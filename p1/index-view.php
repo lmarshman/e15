30bed7db7906049febdd0b73335969741ba5f60c
Laura Marshman <lmarshman@hbs.edu>
2023-02-14 01:45:27 +0000

    created process.php

--- p1/index-view.php
+++ p1/index-view.php
@@ -8,7 +8,23 @@
 </head>
 
 <body>
-    <h1>Laura Marshman Project 1 </h1>
+    <h1>String Processor - e15 Project 1</h1>
+    <h2>By Laura Marshman</h2>
+
+    <h3>Instructions</h3>
+    <p>Enter a word to find out:</p>
+    <ul>
+        <li>Is it a Palindrome? (Same forwards and backwards)</li>
+        <li>How man vowels does it contain</li>
+        <li>What the word would look like if every letter was shifted +1 places in the alphabet</li>
+    </ul>
+    <form method='POST' action='index.php'>
+        <label for='input'>Your word:</label>
+        <input type='text' name='input' id='input'>
+        <button type='submit'>Submit</button>
+    </form>
+
+
 </body>
 
 </html>
