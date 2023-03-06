1365427b942703c368a6eb662732b93c4eb55c7b
Laura Marshman <lmarshman@hbs.edu>
2023-03-06 03:12:17 +0000

    Added Livewire to BookMark Application

--- bookmark/resources/views/pages/welcome.blade.php
+++ bookmark/resources/views/pages/welcome.blade.php
@@ -1,5 +1,8 @@
 @extends('layouts/main')
 
 @section('content')
+    @livewireStyles
+    @livewireScripts
+    <livewire:counter />
     <p>Welcome to Bookmark. Please check back later.</p>
 @endsection
