01386cf99b269d1e46120ae30ba91674bbd00852
Laura Marshman <lmarshman@hbs.edu>
2023-03-06 03:14:55 +0000

    Added a qualifer to the counter

--- bookmark/resources/views/pages/welcome.blade.php
+++ bookmark/resources/views/pages/welcome.blade.php
@@ -3,6 +3,7 @@
 @section('content')
     @livewireStyles
     @livewireScripts
+    <p>How many books would you like to read this year?</p>
     <livewire:counter />
     <p>Welcome to Bookmark. Please check back later.</p>
 @endsection
