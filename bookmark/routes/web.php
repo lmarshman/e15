6f534281aa2804a4d643ecc9e1522ac4249c942b
Laura Marshman <lmarshman@hbs.edu>
2023-03-14 00:06:56 +0000

    Updating bookmark app to follow along in class

--- bookmark/routes/web.php
+++ bookmark/routes/web.php
@@ -7,4 +7,13 @@ use App\Http\Controllers\PageController;
 Route::get('/', [PageController:: class, 'welcome']);
 Route::get('/contact', [PageController:: class, 'contact']);
 Route::get('/books', [BookController::class, 'index']);
-Route::get('/books/{title}', [BookController::class, 'show']);
+Route::get('/books/{slug}', [BookController::class, 'show']);
+Route::get('/books/filter/{category}/{subcategory}', [BookController::class, 'filter']);
+
+Route::get('/list', [ListController::class, 'show']);
+
+Route::get('/example', function () {
+    $foo = [1,2,3];
+    Log::info($foo);
+    return view('abc');
+});
